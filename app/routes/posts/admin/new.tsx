64dd9bb33cc278f0fc480ea1ba1087558afa9c41
Breno M. Silva <sbrenomartins@gmail.com>
2022-12-09 21:53:58 -0300

    first commit

---
+++ app/routes/posts/admin/new.tsx
@@ -0,0 +1,102 @@
+import type { ActionArgs } from "@remix-run/node";
+import { redirect, json } from "@remix-run/node";
+import { Form, useActionData, useTransition } from "@remix-run/react";
+import invariant from "tiny-invariant";
+
+import { createPost } from "~/models/post.server";
+
+export const action = async ({ request }: ActionArgs) => {
+  // TODO: Remove me
+  await new Promise((res) => setTimeout(res, 1000));
+  
+  const formData = await request.formData();
+
+  const title = formData.get("title");
+  const slug = formData.get("slug");
+  const markdown = formData.get("markdown");
+
+  const errors = {
+    title: title ? null : "Title is required",
+    slug: slug ? null : "Slug is required",
+    markdown: markdown ? null : "Markdown is required"
+  };
+
+  const hasErrors = Object.values(errors).some(
+    (errorMessage) => errorMessage
+  );
+
+  if (hasErrors) {
+    return json(errors);
+  }
+
+  invariant(
+    typeof title === "string",
+    "title must be a string"
+  );
+  invariant(
+    typeof slug === "string",
+    "slug must be a string"
+  );
+  invariant(
+    typeof markdown === "string",
+    "markdown must be a string"
+  );
+
+  await createPost({ title, slug, markdown });
+
+  return redirect("/posts/admin");
+}
+
+const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
+
+export default function NewPost() {
+  const errors = useActionData<typeof action>();
+
+  const transition = useTransition();
+  const isCreating = Boolean(transition.submission);
+
+  return (
+    <Form method="post">
+      <p>
+        <label htmlFor="title">
+          Post Title:{" "}
+          {errors?.title ? (
+            <em className="text-red-600">{errors.title}</em>
+          ) : null}
+          <input type="text" id="title" name="title" className={inputClassName} />
+        </label>
+      </p>
+
+      <p>
+        <label htmlFor="slug">
+          Post Slug:{" "}
+          {errors?.slug ? (
+            <em className="text-red-600">{errors.slug}</em>
+          ) : null}
+          <input type="text" id="slug" name="slug" className={inputClassName} />
+        </label>
+      </p>
+
+      <p>
+        <label htmlFor="markdown">
+          Markdown:{" "}
+          {errors?.markdown ? (
+            <em className="text-red-600">{errors.markdown}</em>
+          ) : null}
+          <br />
+        </label>
+          <textarea id="markdown" rows={20} name="markdown" className={`${inputClassName} font-mono`} />
+      </p>
+
+      <p className="text-right">
+        <button 
+          type="submit" 
+          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          disabled={isCreating}
+        >
+          {isCreating ? "Creating..." : "Create Post"}
+        </button>
+      </p>
+    </Form>
+  );
+}
